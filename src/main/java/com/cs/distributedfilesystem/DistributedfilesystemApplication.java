4df9063c00dd1f595f9648e1d5ce846c2e74ad39
claytonleonardcook <claytonleonardcook+github@gmail.com>
2023-10-30 16:03:03 -0400

    init

---
+++ src/main/java/com/cs/distributedfilesystem/DistributedfilesystemApplication.java
@@ -0,0 +1,13 @@
+package com.cs.distributedfilesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedfilesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedfilesystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs/distributedfilesystem/DistributedfilesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.distributedfilesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedfilesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
