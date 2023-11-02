022d662f64358775f3c4956e9c1af2e00b91b4a3
Clayton Cook <24993473+claytonleonardcook@users.noreply.github.com>
2023-11-02 18:11:42 -0400

    ip added

--- src/DistributedFileSystem.java
+++ src/DistributedFileSystem.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 public abstract class DistributedFileSystem {
     // 10.23.2.245 Clay Left
     // 10.23.0.82 Clay Right
+    //10.23.0.45 Siona Right :)
     final static String ENV = "PROD";
     final static String OK = "200",
             BADREQUEST = "400",
