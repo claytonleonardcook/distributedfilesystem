128bf5ef01cb2f68a8ace362862f2ef8a243bb69
claytonleonardcook <claytonleonardcook+github@gmail.com>
2023-11-03 21:29:02 -0400

    comment ip address

--- src/IPAddress.java
+++ src/IPAddress.java
@@ -1,7 +1,20 @@
+/**
+* IP & port structure
+* @author Clayton Cook
+* @author Siona Beaudoin
+*/
 public class IPAddress {
+    /** IP address */
     public String address;
+    /** Port */
     public int port;
 
+    /**
+     * Initialize class attributes
+     * @param address IP address
+     * @param port Port
+     * @throws Exception
+     */
     public IPAddress(String address, int port) {
         this.address = address;
         this.port = port;
